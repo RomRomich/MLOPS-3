1715f4cf1f275541fc6352d747ba6900913f634e
RomRomich <121679425+RomRomich@users.noreply.github.com>
2023-11-23 10:16:37 +0300

    mlflow
    mlflow

---
+++ mlflow/2/6a60e06741404043a620eca2b4eff6a8/artifacts/train_model code/train_model.py
@@ -0,0 +1,32 @@
+# Импорт библиотек
+from sklearn.linear_model import LinearRegression
+import pickle
+import pandas as pd
+import os
+ 
+import mlflow
+from mlflow.tracking import MlflowClient
+
+# Установка URI для отслеживания экспериментов в MLflow
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("train_model")
+
+# Чтение данных из CSV-файла в объект DataFrame
+df = pd.read_csv('/home/admin123/xFlow/datasets/data_train.csv', header=None)
+df.columns = ['id', 'counts']
+model = LinearRegression()
+
+# Работа с MLFlow
+with mlflow.start_run():
+    mlflow.sklearn.log_model(model,
+                             artifact_path="lr",
+                             registered_model_name="lr")
+    mlflow.log_artifact(local_path="/home/admin123/xFlow/scripts/train_model.py",
+                        artifact_path="train_model code")
+    mlflow.end_run()
+ 
+model.fit(df['id'].values.reshape(-1,1), df['counts'])
+
+# Запись модели в файл
+with open('/home/admin123/xFlow/models/data.pickle', 'wb') as f:
+    pickle.dump(model, f)
